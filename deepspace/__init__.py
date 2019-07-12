f166bed3e0590d30a8b9dcef9762dd44633634ac
AfonsoV <bribeiro@dhcp188.strw.leidenuniv.nl>
2019-07-12 11:50:52 +0200

    Updated module loading

--- deepspace/__init__.py
+++ deepspace/__init__.py
@@ -1,7 +1,3 @@
-import os
 from .errors import deepspaceError
-
-DataFolder = os.getenv("DEEPSPACE")
-
-if DataFolder is None:
-    raise deepspaceError("DEEPSPACE environment variable not set.")
+from .deepspace import DeepSpaceData
+from .info import DeepSpaceInfo
